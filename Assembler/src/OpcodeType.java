ec2ed8c8d91369b595812116d7f24ff8fb4cc476
Nate Robinson <30881124+IamKingofNothing@users.noreply.github.com>
2019-01-31 16:01:58 -0600

    Rebuilt as NB project

--- Assembler/src/OpcodeType.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author Nathan Robinson
- */
-
-public enum OpcodeType {
-    HALT, PUSH, RVALUE, LVALUE, POP, STO, COPY, ADD, SUB, MPY, DIV, MOD, NEG, NOT,
-    OR, AND, EQ, NE, GT, GE, LT, LE, LABEL, GOTO, GOFALSE, GOTRUE, PRINT, READ, GOSUB, RET
-}
